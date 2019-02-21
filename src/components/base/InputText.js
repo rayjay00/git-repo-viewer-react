d5e84f6c28af8c7fc6bda8cc8b9ac8a6ba3bbb03
Ryan <hello@ryanjordal.me>
2019-02-21 10:48:00 -0500

    input text

---
+++ src/components/base/InputText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyledInput } from '../../styles/base.js';
+
+
+const InputText = () => {
+    return (
+        <StyledInput type="text" id="user" />
+    );
+}
+
+export default InputText;
