bcb3d1eedc245d8e609bb7048c43b846e994153f
Ryan <hello@ryanjordal.me>
2019-02-24 19:34:00 -0500

    moved styled component and added checked prop

--- src/components/base/Button.js
+++ src/components/base/Button.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import { StyledButton } from '../../../src/styles/base';
 import PropTypes from 'prop-types';
-
-const StyledButton = styled.button`
-    background-color: ${(props) => props.error ? "red" : "#000000" };
-    border-radius: 0;
-    border: 0;
-    font-family: 'Asap', sans-serif;
-    color: white;
-    font-size: 1rem;
-    text-transform: capitalize;
-    margin: 0;
-    height: 2.3rem;
-    flex: 0.6;
-`;
 
 //card prop is going to be true or false and will let the button know if it's part of the github project card
 const Button = ( {error, checked, type, card, handler }) => {
     return (
-        <StyledButton onClick={ handler } type={ type } error={ error } id="search-btn">
+        <StyledButton onClick={ handler } type={ type } error={ error } checked={ checked }id="search-btn">
             {card ? 'See on GitHub' :
             (checked ? 'Search Again' : 'Search')}
         </StyledButton>
