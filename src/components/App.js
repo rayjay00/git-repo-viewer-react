fe571e0da07f8e4f71488974372d4d83bea26f2d
Ryan <hello@ryanjordal.me>
2019-03-31 19:40:47 -0400

    major changes to default state and in render

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './home/Header';
 import Content from './home/Content';
 import { StyledHeading } from '../styles/base';
-import axios from 'axios';
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            repos: [],
-            username: "",
             checked: false,
+            user: "",
+            repos: [],
             error: false,
-            owner: "",
-            value: "",
-            loading: false
+            loading: false,
+            avatar: "",
+            value: ""
         };
     }
     
-    //this handler is passed down to the to the InputText component 
-    getRepos = () => {
-        this.setState({
-            loading: true
-        });
-        //is there a way to grab username from state? when i trie, I had to click the submit button twice
-        axios.get(`https://api.github.com/users/${this.state.value}/repos`)
-        
+    //This function is passed to the to the Submit component as a prop and is called within a function in that file that uses the text input ref as a parameter. 
+    getRepos = username => {
+        axios.get(`https://api.github.com/users/${username}/repos`)
         .then(res => {
             const repos = res.data;
             console.log('repos', repos);
-            //I thought about rendering the state in another "then"
             this.setState({
-                //I'm still debating this next line
-                repos: repos,
-                username: "",
-                user: repos[0]["owner"]["login"],
                 checked: true,
+                user: repos[0]["owner"]["login"],
+                repos: repos,
                 error: false,
-                owner: repos[0]["owner"],
-                avatar: repos[0]["owner"]["avatar_url"],
-                loading: false
+                avatar: repos[0]["owner"]["avatar_url"]
             })
         })
-        .catch((error) => {
+        .catch( error => {
             console.log("error", error);
             this.setState({
                 error: true,
-                loading: false
+                loading: false,
+                avatar: ""
             })
         })
     }
 
-    addUsername = (e) => {
+    addUsername = userinput => {
         this.setState({
-            value: e.target.value
+            value: userinput
         })
     }
    
     render() {
-        const {repos, checked, error, user, avatar, value} = this.state;
+        const { checked, error, avatar, user, repos } = this.state;
         return (
             <> 
-                <Header action={ this.addUsername } value={ value } title={user && !error ? user : "Search for a GitHub User"} checked={ checked } error={ error } source={ checked && !error ? avatar : "https://github.githubassets.com/images/modules/logos_page/Octocat.png" } handler={ this.getRepos }/>
-                { error ?
-                    <StyledHeading error={ error }>Oops... Looks like that user doesn't exist. Try again!</StyledHeading>
+                <Header 
+                source={ checked && !error ? avatar : "https://github.githubassets.com/images/modules/logos_page/Octocat.png" } 
+                title={ user && !error ? user : "Search for a GitHub User" } 
+                addUsername={ this.addUsername } 
+                checked={ checked } 
+                error={ error } 
+                getRepos={ this.getRepos }/>
+                { error ? 
+                    <StyledHeading>Oops! It looks like that user doesn't exist.</StyledHeading>
                     :
                     <>
-                        { repos.length > 1 && <Content repos={ repos } /> }
+                        { repos.length > 0 && <Content repos={ repos } /> }
                     </>
                 }
             </>
