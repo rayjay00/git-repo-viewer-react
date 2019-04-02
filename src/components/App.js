4ce00fdb931555f7b3c99805987edb90c14102dc
Ryan <hello@ryanjordal.me>
2019-04-02 08:35:03 -0400

    removed token

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,10 @@ import {ApolloProvider} from "react-apollo";
 const client = new ApolloClient ({
     uri: 'https://api.github.com/graphql',
     request: async operation => {
-        // const token = await  aed762f3c4f7e021047d8673bcc5d5261157ded9 
+        // const token = await   
         operation.setContext({
             headers: {
-                authorization: `token aed762f3c4f7e021047d8673bcc5d5261157ded9`
+                authorization: `token `
             }
         });
     }
